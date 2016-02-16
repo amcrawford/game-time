e40edba748cb305ed5a58770891a654049f11842
Amber Crawford <ammcrawford@gmail.com>
2016-02-16 13:38:58 -0700

    Snake does not die when hits self

---
+++ test/game-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const Game = require('../lib/game');
+const Snake = require('../lib/snake');
+
+
+describe('Game', function() {
+  it('should end when snake intersects its own body', function(){
+    
+  })
+
+}
