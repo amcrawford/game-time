2901af4999278a6ad4547e37c02f6ee00631ed53
Amber Crawford <ammcrawford@gmail.com>
2016-02-18 11:57:48 -0700

    Resolve jshint errors

--- test/round-test.js
+++ test/round-test.js
@@ -36,81 +36,81 @@ describe('Round', function() {
   describe('detects snake and pellet intersection', function(){
     it('should return true if snake "eats" pellet', function(){
       var round = new Round({});
-      round.snake.positions = [[1,1], [12,12]]
-      round.pellet.x = 1
-      round.pellet.y = 1
+      round.snake.positions = [[1,1], [12,12]];
+      round.pellet.x = 1;
+      round.pellet.y = 1;
 
-      assert(round.snakeIntersectsPellet())
-    })
+      assert(round.snakeIntersectsPellet());
+    });
 
     it('should return true if snake intersects pellet indirectly', function(){
       var round = new Round({});
-      round.snake.positions = [[1,1], [12,12]]
-      round.pellet.x = 1
-      round.pellet.y = 9
+      round.snake.positions = [[1,1], [12,12]];
+      round.pellet.x = 1;
+      round.pellet.y = 9;
 
-      assert(round.snakeIntersectsPellet())
-    })
+      assert(round.snakeIntersectsPellet());
+    });
 
     it('should return false if snake does not pellet', function(){
       var round = new Round({});
-      round.snake.positions = [[1,1], [12,12]]
-      round.pellet.x = 24
-      round.pellet.y = 24
+      round.snake.positions = [[1,1], [12,12]];
+      round.pellet.x = 24;
+      round.pellet.y = 24;
 
-      assert(!round.snakeIntersectsPellet())
-    })
-  })
+      assert(!round.snakeIntersectsPellet());
+    });
+  });
 
   describe('detects intersection with wall', function(){
     it('should return true if snake runs into left wall', function(){
       var round = new Round({});
-      round.snake.positions = [[-1,1], [10,10]]
+      round.snake.positions = [[-1,1], [10,10]];
 
-      assert(round.snakeIntersectsWall())
-    })
+      assert(round.snakeIntersectsWall());
+    });
 
     it('should return true if snake runs into right wall', function(){
       var round = new Round({});
-      round.snake.positions = [[500,1], [490,1]]
+      round.snake.positions = [[500,1], [490,1]];
 
-      assert(round.snakeIntersectsWall())
-    })
+      assert(round.snakeIntersectsWall());
+    });
 
     it('should return true if snake runs into top wall', function(){
       var round = new Round({});
-      round.snake.positions = [[10,-1], [19,1]]
+      round.snake.positions = [[10,-1], [19,1]];
 
-      assert(round.snakeIntersectsWall())
-    })
+      assert(round.snakeIntersectsWall());
+    });
 
     it('should return true if snake runs into down wall', function(){
       var round = new Round({});
-      round.snake.positions = [[10, 410], [10,400]]
+      round.snake.positions = [[10, 410], [10,400]];
 
-      assert(round.snakeIntersectsWall())
-    })
+      assert(round.snakeIntersectsWall());
+    });
 
     it('should return false if snake is within canvas', function(){
       var round = new Round({});
-      round.snake.positions = [[10,10], [19,10]]
+      round.snake.positions = [[10,10], [19,10]];
 
-      assert(!round.snakeIntersectsWall())
-    })
-  })
+      assert(!round.snakeIntersectsWall());
+    });
+  });
 
   describe('prevents pellet from overlapping with snake on reset', function(){
     it('should not exit method if pellet landed on snake', function(){
       var round = new Round({});
 
-      round.snake.positions = [[1,1], [12,12]]
-      round.pellet.x = 10
-      round.pellet.y = 9
+      round.snake.positions = [[1,1], [12,12]];
+      round.pellet.x = 10;
+      round.pellet.y = 9;
 
       round.doesPelletOverlapWithSnake();
 
-      assert(round.pellet.x !== 10 && round.pellet.y !== 9)
+      assert(round.pellet.x !== 10 && round.pellet.y !== 9);
 
-    })
-  })
+    });
+  });
 });
